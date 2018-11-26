bd196fff355570c152847d6305f469a80622cea8
anriku <1039323669@qq.com>
2018-11-27 00:41:28 +0800

    复杂链表复制、二叉树路径

---
+++ src/chapter4/CloneLinkedList.java
@@ -0,0 +1,88 @@
+package chapter4;
+
+/**
+ * 复制复杂链表
+ */
+public class CloneLinkedList {
+
+
+    public static void main(String[] args) {
+        Node<Integer> head = new Node<>(1);
+        head.next = new Node<>(2);
+        head.next.next = new Node<>(10);
+        head.next.next.next = new Node<>(12);
+        head.next.next.next.next = new Node<>(5);
+        head.next.next.next.next.next = null;
+
+        head.sibling = head.next.next.next;
+
+        Node<Integer> ret = cloneList(head);
+
+        while (ret != null) {
+            System.out.println(ret.value);
+            if (ret.sibling != null){
+                System.out.println("sibling " + ret.sibling.value);
+            }
+            ret = ret.next;
+        }
+    }
+
+    public static <T> Node<T> cloneList(Node<T> head) {
+        cloneNodes(head);
+        connectSibling(head);
+        return getClonedList(head);
+    }
+
+    public static <T> void cloneNodes(Node<T> head) {
+        Node<T> node = head;
+        while (node != null) {
+            Node<T> clonedNode = new Node<>(node.value);
+            clonedNode.next = node.next;
+            clonedNode.sibling = null;
+            node.next = clonedNode;
+            node = clonedNode.next;
+        }
+    }
+
+    public static <T> void connectSibling(Node<T> head) {
+        Node<T> node = head;
+        Node<T> clonedNode;
+
+        while (node != null) {
+            clonedNode = node.next;
+            if (node.sibling != null) {
+                clonedNode.sibling = node.sibling.next;
+            }
+            node = clonedNode.next;
+        }
+    }
+
+    public static <T> Node<T> getClonedList(Node<T> head) {
+        Node<T> node = head;
+        Node<T> clonedHead = null;
+        Node<T> clonedNode = null;
+
+        if (node != null) {
+            clonedHead = clonedNode = node.next;
+            node = node.next.next;
+        }
+
+        while (node != null) {
+            clonedNode.next = node.next;
+            clonedNode = clonedNode.next;
+            node = node.next.next;
+        }
+        return clonedHead;
+    }
+
+    static class Node<T> {
+        T value;
+        Node<T> next;
+        Node<T> sibling;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+}
---
+++ src/chapter4/FindPath.java
@@ -0,0 +1,60 @@
+package chapter4;
+
+import java.util.Stack;
+
+/**
+ * 给定某个二叉树以及一个数字和。得到某个节点使其路径和为给定数字和
+ */
+public class FindPath {
+
+    public static void main(String[] args) {
+        BinaryTreeNode root = new BinaryTreeNode(10);
+        root.left = new BinaryTreeNode(5);
+        root.right = new BinaryTreeNode(12);
+        root.left.left = new BinaryTreeNode(4);
+        root.left.right = new BinaryTreeNode(7);
+
+        findPath(root, 19);
+    }
+
+    public static void findPath(BinaryTreeNode root, int expectedNum) {
+        if (root == null) {
+            return;
+        }
+        Stack<Integer> stack = new Stack<>();
+
+        findPath(root, stack, 0, expectedNum);
+    }
+
+    public static void findPath(BinaryTreeNode root, Stack<Integer> stack, int currentSum, int expectedNum) {
+        stack.push(root.value);
+        currentSum += root.value;
+        boolean isLeaf = (root.left == null) && (root.right == null);
+
+        if (isLeaf && currentSum == expectedNum) {
+            for (int i : stack) {
+                System.out.println(i);
+            }
+        }
+
+        if (root.left != null) {
+            findPath(root.left, stack, currentSum, expectedNum);
+        }
+
+        if (root.right != null) {
+            findPath(root.right, stack, currentSum, expectedNum);
+        }
+        int i = stack.pop();
+    }
+
+    static class BinaryTreeNode {
+
+        int value;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+
+        public BinaryTreeNode(int value) {
+            this.value = value;
+        }
+    }
+}
